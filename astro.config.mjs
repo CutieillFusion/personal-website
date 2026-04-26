0a22cbb1f57596fe3e8302464a0dbd0167560135
Dylan Norquist <norquistd@msoe.edu>
2026-04-26 17:13:42 -0500

    adding llms.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,9 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://dylannorquist.codes',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/blog'),
+    }),
+  ],
 });
